ee2752a1caf1f4a7e17122685bdece25fbde634d
Gianluca Iannuzzi <giannuzzi2001@gmail.com>
2025-11-04 23:34:45 -0300

    Se limpia el proyecto. Se agregan configuraciones basicas. Se deja listo mainc.c para comenzar el desarrollo.

--- TP-CasaInteligente/FreeRTOSConfig.h
+++ TP-CasaInteligente/FreeRTOSConfig.h
@@ -28,25 +28,26 @@
 #ifndef FREERTOS_CONFIG_H
 #define FREERTOS_CONFIG_H
 
-/*-----------------------------------------------------------
- * Application specific definitions.
- *
- * These definitions should be adjusted for your particular hardware and
- * application requirements.
- *
- * THESE PARAMETERS ARE DESCRIBED WITHIN THE 'CONFIGURATION' SECTION OF THE
- * FreeRTOS API DOCUMENTATION AVAILABLE ON THE FreeRTOS.org WEB SITE.  See
- * https://www.FreeRTOS.org/a00110.html
- *----------------------------------------------------------*/
+ /*-----------------------------------------------------------
+  * Application specific definitions.
+  *
+  * These definitions should be adjusted for your particular hardware and
+  * application requirements.
+  *
+  * THESE PARAMETERS ARE DESCRIBED WITHIN THE 'CONFIGURATION' SECTION OF THE
+  * FreeRTOS API DOCUMENTATION AVAILABLE ON THE FreeRTOS.org WEB SITE.  See
+  * https://www.FreeRTOS.org/a00110.html
+  *----------------------------------------------------------*/
 
 #define configUSE_PREEMPTION					1
-#define configUSE_PORT_OPTIMISED_TASK_SELECTION	1
-#define configUSE_IDLE_HOOK						1
-#define configUSE_TICK_HOOK						1
+#define configUSE_PORT_OPTIMISED_TASK_SELECTION	0
+#define configUSE_IDLE_HOOK						0
+#define configUSE_TICK_HOOK						0
+#define configUSE_NEWLIB_REENTRANT              0
 #define configUSE_DAEMON_TASK_STARTUP_HOOK		1
 #define configTICK_RATE_HZ						( 1000 ) /* In this non-real time simulated environment the tick frequency has to be at least a multiple of the Win32 tick frequency, and therefore very slow. */
-#define configMINIMAL_STACK_SIZE				( ( unsigned short ) 70 ) /* In this simulated case, the stack only has to hold one small structure as the real stack is part of the win32 thread. */
-#define configTOTAL_HEAP_SIZE					( ( size_t ) ( 490 * 1024 ) ) /* This demo tests heap_5 so places multiple blocks within this total heap size.  See mainREGION_1_SIZE to mainREGION_3_SIZE definitions in main.c. */
+#define configMINIMAL_STACK_SIZE				( 128 ) /* In this simulated case, the stack only has to hold one small structure as the real stack is part of the win32 thread. */
+#define configTOTAL_HEAP_SIZE					( ( size_t ) ( 64 * 1024 ) ) /* This demo tests heap_5 so places multiple blocks within this total heap size.  See mainREGION_1_SIZE to mainREGION_3_SIZE definitions in main.c. */
 #define configMAX_TASK_NAME_LEN					( 12 )
 #define configUSE_TRACE_FACILITY				1
 #define configIDLE_SHOULD_YIELD					1
@@ -60,30 +61,31 @@
 #define configUSE_ALTERNATIVE_API				0
 #define configUSE_QUEUE_SETS					1
 #define configUSE_TASK_NOTIFICATIONS			1
-#define configTASK_NOTIFICATION_ARRAY_ENTRIES		5
-#define configSUPPORT_STATIC_ALLOCATION			1
+#define configTASK_NOTIFICATION_ARRAY_ENTRIES   5
+#define configSUPPORT_STATIC_ALLOCATION			0
+#define configSUPPORT_DYNAMIC_ALLOCATION        1
 #define configINITIAL_TICK_COUNT				( ( TickType_t ) 0 ) /* For test. */
 #define configSTREAM_BUFFER_TRIGGER_LEVEL_TEST_MARGIN 1 /* As there are a lot of tasks running. */
 
- /* Tick type width is defined based on the target platform(32bit or 64bit). */
+  /* Tick type width is defined based on the target platform(32bit or 64bit). */
 #ifdef _M_X64
-    #define configTICK_TYPE_WIDTH_IN_BITS		TICK_TYPE_WIDTH_64_BITS
+#define configTICK_TYPE_WIDTH_IN_BITS		TICK_TYPE_WIDTH_64_BITS
 #else
-    #define configTICK_TYPE_WIDTH_IN_BITS		TICK_TYPE_WIDTH_32_BITS
+#define configTICK_TYPE_WIDTH_IN_BITS		TICK_TYPE_WIDTH_32_BITS
 #endif
 
 /* Software timer related configuration options. */
 #define configUSE_TIMERS						1
 #define configTIMER_TASK_PRIORITY				( configMAX_PRIORITIES - 1 )
-#define configTIMER_QUEUE_LENGTH				20
-#define configTIMER_TASK_STACK_DEPTH			( configMINIMAL_STACK_SIZE * 2 )
+#define configTIMER_QUEUE_LENGTH				5
+#define configTIMER_TASK_STACK_DEPTH			( 512 )
 
 #define configMAX_PRIORITIES					( 7 )
 
 /* Run time stats gathering configuration options. */
 #define configRUN_TIME_COUNTER_TYPE				uint64_t
-configRUN_TIME_COUNTER_TYPE ulGetRunTimeCounterValue( void ); /* Prototype of function that returns run time counter. */
-void vConfigureTimerForRunTimeStats( void );	/* Prototype of function that initialises the run time counter. */
+configRUN_TIME_COUNTER_TYPE ulGetRunTimeCounterValue(void); /* Prototype of function that returns run time counter. */
+void vConfigureTimerForRunTimeStats(void);	/* Prototype of function that initialises the run time counter. */
 #define configGENERATE_RUN_TIME_STATS			1
 #define portCONFIGURE_TIMER_FOR_RUN_TIME_STATS() vConfigureTimerForRunTimeStats()
 #define portGET_RUN_TIME_COUNTER_VALUE() ulGetRunTimeCounterValue()
@@ -122,21 +124,21 @@ functions anyway. */
  * time. */
 #define configRUN_ADDITIONAL_TESTS				1
 
-/* It is a good idea to define configASSERT() while developing.  configASSERT()
-uses the same semantics as the standard C assert() macro. */
-extern void vAssertCalled( unsigned long ulLine, const char * const pcFileName );
+ /* It is a good idea to define configASSERT() while developing.  configASSERT()
+ uses the same semantics as the standard C assert() macro. */
+extern void vAssertCalled(unsigned long ulLine, const char* const pcFileName);
 #define configASSERT( x ) if( ( x ) == 0 ) vAssertCalled( __LINE__, __FILE__ )
 
 #define configINCLUDE_MESSAGE_BUFFER_AMP_DEMO	0
 #if ( configINCLUDE_MESSAGE_BUFFER_AMP_DEMO == 1 )
-	extern void vGenerateCoreBInterrupt( void * xUpdatedMessageBuffer );
-	#define sbSEND_COMPLETED( pxStreamBuffer ) vGenerateCoreBInterrupt( pxStreamBuffer )
+extern void vGenerateCoreBInterrupt(void* xUpdatedMessageBuffer);
+#define sbSEND_COMPLETED( pxStreamBuffer ) vGenerateCoreBInterrupt( pxStreamBuffer )
 #endif /* configINCLUDE_MESSAGE_BUFFER_AMP_DEMO */
 
 #ifdef WIN32_LEAN_AND_MEAN
-    #include "winsock2.h"
+#include "winsock2.h"
 #else
-    #include <winsock.h>
+#include <winsock.h>
 #endif /* WIN32_LEAN_AND_MEAN */
 
 /* Include the FreeRTOS+Trace FreeRTOS trace macro definitions. */
